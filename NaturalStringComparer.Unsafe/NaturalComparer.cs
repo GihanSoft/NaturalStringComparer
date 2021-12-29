e026d87ef07648661022ec6cb0caec31fcf68e8c
Muhammad Babayi <chief.mb1976@gmail.com>
2021-12-29 08:32:43 +0330

    remove `stylecope` file

--- NaturalStringComparer.Unsafe/NaturalComparer.cs
+++ NaturalStringComparer.Unsafe/NaturalComparer.cs
@@ -5,146 +5,149 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace GihanSoft.String
+namespace GihanSoft.String;
+
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Natural Comparer.
+/// </summary>
+public class NaturalComparer : IComparer<string?>
 {
-    using System;
-    using System.Collections.Generic;
+    private static readonly Lazy<NaturalComparer> LazyCurrentCulture = new(() => new NaturalComparer(StringComparison.CurrentCulture));
+    private static readonly Lazy<NaturalComparer> LazyCurrentCultureIgnoreCase = new(() => new NaturalComparer(StringComparison.CurrentCultureIgnoreCase));
+#if !NETSTANDARD1_0
+    private static readonly Lazy<NaturalComparer> LazyInvariantCulture = new (() => new NaturalComparer(StringComparison.InvariantCulture));
+    private static readonly Lazy<NaturalComparer> LazyInvariantCultureIgnoreCase = new (() => new NaturalComparer(StringComparison.InvariantCultureIgnoreCase));
+#endif
+    private static readonly Lazy<NaturalComparer> LazyOrdinal = new(() => new NaturalComparer(StringComparison.Ordinal));
+    private static readonly Lazy<NaturalComparer> LazyOrdinalIgnoreCase = new(() => new NaturalComparer(StringComparison.OrdinalIgnoreCase));
+
+    private readonly StringComparison stringComparison;
 
     /// <summary>
-    /// Natural Comparer.
+    /// Initializes a new instance of the <see cref="NaturalComparer"/> class.
     /// </summary>
-    public class NaturalComparer : IComparer<string?>
+    /// <param name="stringComparison">string comparison mode.</param>
+    public NaturalComparer(StringComparison stringComparison = StringComparison.Ordinal)
     {
-        private static readonly Lazy<NaturalComparer> LazyCurrentCulture = new(() => new NaturalComparer(StringComparison.CurrentCulture));
-        private static readonly Lazy<NaturalComparer> LazyCurrentCultureIgnoreCase = new(() => new NaturalComparer(StringComparison.CurrentCultureIgnoreCase));
+        this.stringComparison = stringComparison;
+    }
+
+    /// <summary>
+    /// Gets default Comparer that uses <see cref="StringComparison.CurrentCulture"/>.
+    /// </summary>
+    public static NaturalComparer CurrentCulture => LazyCurrentCulture.Value;
+
+    /// <summary>
+    /// Gets default Comparer that uses <see cref="StringComparison.CurrentCultureIgnoreCase"/>.
+    /// </summary>
+    public static NaturalComparer CurrentCultureIgnoreCase => LazyCurrentCultureIgnoreCase.Value;
+
 #if !NETSTANDARD1_0
-        private static readonly Lazy<NaturalComparer> LazyInvariantCulture = new (() => new NaturalComparer(StringComparison.InvariantCulture));
-        private static readonly Lazy<NaturalComparer> LazyInvariantCultureIgnoreCase = new (() => new NaturalComparer(StringComparison.InvariantCultureIgnoreCase));
+
+    /// <summary>
+    /// Gets default Comparer that uses <see cref="StringComparison.InvariantCulture"/>.
+    /// </summary>
+    public static NaturalComparer InvariantCulture => LazyInvariantCulture.Value;
+
+    /// <summary>
+    /// Gets default Comparer that uses <see cref="StringComparison.InvariantCultureIgnoreCase"/>.
+    /// </summary>
+    public static NaturalComparer InvariantCultureIgnoreCase => LazyInvariantCultureIgnoreCase.Value;
+
 #endif
-        private static readonly Lazy<NaturalComparer> LazyOrdinal = new(() => new NaturalComparer(StringComparison.Ordinal));
-        private static readonly Lazy<NaturalComparer> LazyOrdinalIgnoreCase = new(() => new NaturalComparer(StringComparison.OrdinalIgnoreCase));
 
-        private readonly StringComparison stringComparison;
+    /// <summary>
+    /// Gets default Comparer that uses <see cref="StringComparison.Ordinal"/>.
+    /// </summary>
+    public static NaturalComparer Ordinal => LazyOrdinal.Value;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="NaturalComparer"/> class.
-        /// </summary>
-        /// <param name="stringComparison">string comparison mode.</param>
-        public NaturalComparer(StringComparison stringComparison = StringComparison.Ordinal)
+    /// <summary>
+    /// Gets default Comparer that uses <see cref="StringComparison.OrdinalIgnoreCase"/>.
+    /// </summary>
+    public static NaturalComparer OrdinalIgnoreCase => LazyOrdinalIgnoreCase.Value;
+
+    /// <summary>
+    /// Compares 2 <see cref="string"/>.
+    /// </summary>
+    /// <param name="x">1st string.</param>
+    /// <param name="y">2nd string.</param>
+    /// <param name="stringComparison"><see cref="StringComparison"/> mode.</param>
+    /// <returns>value that show comparison result.</returns>
+    public static int Compare(string? x, string? y, StringComparison stringComparison)
+    {
+        if (ReferenceEquals(x, y))
         {
-            this.stringComparison = stringComparison;
+            return 0;
         }
 
-        /// <summary>
-        /// Gets default Comparer that uses <see cref="StringComparison.CurrentCulture"/>.
-        /// </summary>
-        public static NaturalComparer CurrentCulture => LazyCurrentCulture.Value;
-
-        /// <summary>
-        /// Gets default Comparer that uses <see cref="StringComparison.CurrentCultureIgnoreCase"/>.
-        /// </summary>
-        public static NaturalComparer CurrentCultureIgnoreCase => LazyCurrentCultureIgnoreCase.Value;
-
-#if !NETSTANDARD1_0
-        /// <summary>
-        /// Gets default Comparer that uses <see cref="StringComparison.InvariantCulture"/>.
-        /// </summary>
-        public static NaturalComparer InvariantCulture => LazyInvariantCulture.Value;
-
-        /// <summary>
-        /// Gets default Comparer that uses <see cref="StringComparison.InvariantCultureIgnoreCase"/>.
-        /// </summary>
-        public static NaturalComparer InvariantCultureIgnoreCase => LazyInvariantCultureIgnoreCase.Value;
-#endif
-        /// <summary>
-        /// Gets default Comparer that uses <see cref="StringComparison.Ordinal"/>.
-        /// </summary>
-        public static NaturalComparer Ordinal => LazyOrdinal.Value;
-
-        /// <summary>
-        /// Gets default Comparer that uses <see cref="StringComparison.OrdinalIgnoreCase"/>.
-        /// </summary>
-        public static NaturalComparer OrdinalIgnoreCase => LazyOrdinalIgnoreCase.Value;
-
-        /// <summary>
-        /// Compares 2 <see cref="string"/>.
-        /// </summary>
-        /// <param name="x">1st string.</param>
-        /// <param name="y">2nd string.</param>
-        /// <param name="stringComparison"><see cref="StringComparison"/> mode.</param>
-        /// <returns>value that show comparison result.</returns>
-        public static int Compare(string? x, string? y, StringComparison stringComparison)
+        if (x is null)
         {
-            if (ReferenceEquals(x, y))
-            {
-                return 0;
-            }
+            return -1;
+        }
 
-            if (x is null)
-            {
-                return -1;
-            }
+        if (y is null)
+        {
+            return 1;
+        }
 
-            if (y is null)
+        unsafe
+        {
+            fixed (char* xp = x, yp = y)
             {
-                return 1;
-            }
+                var xPointer = xp;
+                var yPointer = yp;
 
-            unsafe
-            {
-                fixed (char* xp = x, yp = y)
+                while (*xPointer != 0)
                 {
-                    var xPointer = xp;
-                    var yPointer = yp;
-
-                    while (*xPointer != 0)
+                    if (*yPointer == 0)
                     {
-                        if (*yPointer == 0)
-                        {
-                            return 1;
-                        }
-
-                        if (char.IsDigit(*xPointer) && char.IsDigit(*yPointer))
-                        {
-                            var xNum = GetNumber(&xPointer);
-                            var yNum = GetNumber(&yPointer);
-                            if (xNum != yNum)
-                            {
-                                return xNum > yNum ? 1 : -1;
-                            }
-                        }
-
-                        if (*xPointer != *yPointer)
-                        {
-                            return string.Compare((*xPointer).ToString(), (*yPointer).ToString(), stringComparison);
-                        }
-
-                        yPointer++;
-                        xPointer++;
+                        return 1;
                     }
 
-                    return *yPointer == 0 ? 0 : -1;
+                    if (char.IsDigit(*xPointer) && char.IsDigit(*yPointer))
+                    {
+                        var xNum = GetNumber(&xPointer);
+                        var yNum = GetNumber(&yPointer);
+                        if (xNum != yNum)
+                        {
+                            return xNum > yNum ? 1 : -1;
+                        }
+                    }
+
+                    if (*xPointer != *yPointer)
+                    {
+                        return string.Compare((*xPointer).ToString(), (*yPointer).ToString(), stringComparison);
+                    }
+
+                    yPointer++;
+                    xPointer++;
                 }
+
+                return *yPointer == 0 ? 0 : -1;
             }
         }
+    }
 
-        /// <summary>
-        /// Compares 2 <see cref="string"/>.
-        /// </summary>
-        /// <param name="x">1st string.</param>
-        /// <param name="y">2nd string.</param>
-        /// <returns>value that show comparison result.</returns>
-        public int Compare(string? x, string? y) => Compare(x, y, stringComparison);
+    /// <summary>
+    /// Compares 2 <see cref="string"/>.
+    /// </summary>
+    /// <param name="x">1st string.</param>
+    /// <param name="y">2nd string.</param>
+    /// <returns>value that show comparison result.</returns>
+    public int Compare(string? x, string? y) => Compare(x, y, this.stringComparison);
 
-        private static unsafe int GetNumber(char** pointer)
+    private static unsafe int GetNumber(char** pointer)
+    {
+        var number = 0;
+        while (**pointer != 0 && char.IsDigit(**pointer))
         {
-            var number = 0;
-            while (**pointer != 0 && char.IsDigit(**pointer))
-            {
-                number = (10 * number) + Convert.ToByte((char)(**pointer - 48));
-                (*pointer)++;
-            }
-            return number;
+            number = (10 * number) + Convert.ToByte((char)(**pointer - 48));
+            (*pointer)++;
         }
+
+        return number;
     }
 }
--- NaturalStringComparer.Unsafe/NaturalComparerExtensions.cs
+++ NaturalStringComparer.Unsafe/NaturalComparerExtensions.cs
@@ -5,85 +5,84 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace GihanSoft.String
+namespace GihanSoft.String;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// NaturalComparer Extensions.
+/// </summary>
+public static class NaturalComparerExtensions
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
+    /// <summary>
+    /// Naturally sort a list.
+    /// </summary>
+    /// <typeparam name="TSource">List type.</typeparam>
+    /// <param name="src">list to sort.</param>
+    /// <param name="keySelector">key selector that select string to use in natural comparison.</param>
+    /// <param name="stringComparison">base string comparison mode.</param>
+    public static void NaturalSort<TSource>(
+        this List<TSource> src,
+        Func<TSource, string?>? keySelector = null,
+        StringComparison stringComparison = StringComparison.Ordinal)
+    {
+        keySelector ??= o => o?.ToString();
+        src.Sort((x, y) =>
+            NaturalComparer.Compare(keySelector(x), keySelector(y), stringComparison));
+    }
 
     /// <summary>
-    /// NaturalComparer Extensions.
+    /// Naturally sort a list.
     /// </summary>
-    public static class NaturalComparerExtensions
+    /// <typeparam name="TSource">List type.</typeparam>
+    /// <param name="src">list to sort.</param>
+    /// <param name="keySelector">key selector that select string to use in natural comparison.</param>
+    /// <param name="stringComparison">base string comparison mode.</param>
+    public static void NaturalSort<TSource>(
+        this TSource[] src,
+        Func<TSource, string?>? keySelector = null,
+        StringComparison stringComparison = StringComparison.Ordinal)
     {
-        /// <summary>
-        /// Naturally sort a list.
-        /// </summary>
-        /// <typeparam name="TSource">List type.</typeparam>
-        /// <param name="src">list to sort.</param>
-        /// <param name="keySelector">key selector that select string to use in natural comparison.</param>
-        /// <param name="stringComparison">base string comparison mode.</param>
-        public static void NaturalSort<TSource>(
-            this List<TSource> src,
-            Func<TSource, string?>? keySelector = null,
-            StringComparison stringComparison = StringComparison.Ordinal)
-        {
-            keySelector ??= o => o?.ToString();
-            src.Sort((x, y) =>
-                NaturalComparer.Compare(keySelector(x), keySelector(y), stringComparison));
-        }
+        keySelector ??= o => o?.ToString();
+        Array.Sort(src, (x, y) =>
+            NaturalComparer.Compare(keySelector(x), keySelector(y), stringComparison));
+    }
 
-        /// <summary>
-        /// Naturally sort a list.
-        /// </summary>
-        /// <typeparam name="TSource">List type.</typeparam>
-        /// <param name="src">list to sort.</param>
-        /// <param name="keySelector">key selector that select string to use in natural comparison.</param>
-        /// <param name="stringComparison">base string comparison mode.</param>
-        public static void NaturalSort<TSource>(
-            this TSource[] src,
-            Func<TSource, string?>? keySelector = null,
-            StringComparison stringComparison = StringComparison.Ordinal)
-        {
-            keySelector ??= o => o?.ToString();
-            Array.Sort(src, (x, y) =>
-                NaturalComparer.Compare(keySelector(x), keySelector(y), stringComparison));
-        }
+    public static IOrderedEnumerable<TSource> NaturalOrderBy<TSource>(
+        this IEnumerable<TSource> source,
+        Func<TSource, string?>? keySelector = null,
+        StringComparison stringComparison = StringComparison.Ordinal)
+    {
+        keySelector ??= o => o?.ToString();
+        return source.OrderBy(keySelector, new NaturalComparer(stringComparison));
+    }
 
-        public static IOrderedEnumerable<TSource> NaturalOrderBy<TSource>(
-            this IEnumerable<TSource> source,
-            Func<TSource, string?>? keySelector = null,
-            StringComparison stringComparison = StringComparison.Ordinal)
-        {
-            keySelector ??= o => o?.ToString();
-            return source.OrderBy(keySelector, new NaturalComparer(stringComparison));
-        }
+    public static IOrderedEnumerable<TSource> NaturalOrderByDescending<TSource>(
+        this IEnumerable<TSource> source,
+        Func<TSource, string?>? keySelector = null,
+        StringComparison stringComparison = StringComparison.Ordinal)
+    {
+        keySelector ??= o => o?.ToString();
+        return source.OrderByDescending(keySelector, new NaturalComparer(stringComparison));
+    }
 
-        public static IOrderedEnumerable<TSource> NaturalOrderByDescending<TSource>(
-            this IEnumerable<TSource> source,
-            Func<TSource, string?>? keySelector = null,
-            StringComparison stringComparison = StringComparison.Ordinal)
-        {
-            keySelector ??= o => o?.ToString();
-            return source.OrderByDescending(keySelector, new NaturalComparer(stringComparison));
-        }
+    public static IOrderedEnumerable<TSource> NaturalThenBy<TSource>(
+        this IOrderedEnumerable<TSource> source,
+        Func<TSource, string?>? keySelector = null,
+        StringComparison stringComparison = StringComparison.Ordinal)
+    {
+        keySelector ??= o => o?.ToString();
+        return source.ThenBy(keySelector, new NaturalComparer(stringComparison));
+    }
 
-        public static IOrderedEnumerable<TSource> NaturalThenBy<TSource>(
-            this IOrderedEnumerable<TSource> source,
-            Func<TSource, string?>? keySelector = null,
-            StringComparison stringComparison = StringComparison.Ordinal)
-        {
-            keySelector ??= o => o?.ToString();
-            return source.ThenBy(keySelector, new NaturalComparer(stringComparison));
-        }
-
-        public static IOrderedEnumerable<TSource> NaturalThenByDescending<TSource>(
-            this IOrderedEnumerable<TSource> source,
-            Func<TSource, string?>? keySelector = null,
-            StringComparison stringComparison = StringComparison.Ordinal)
-        {
-            keySelector ??= o => o?.ToString();
-            return source.ThenByDescending(keySelector, new NaturalComparer(stringComparison));
-        }
+    public static IOrderedEnumerable<TSource> NaturalThenByDescending<TSource>(
+        this IOrderedEnumerable<TSource> source,
+        Func<TSource, string?>? keySelector = null,
+        StringComparison stringComparison = StringComparison.Ordinal)
+    {
+        keySelector ??= o => o?.ToString();
+        return source.ThenByDescending(keySelector, new NaturalComparer(stringComparison));
     }
 }
